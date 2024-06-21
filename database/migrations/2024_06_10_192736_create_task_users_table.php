7eb611ba66aedc1927b51bc08776d49701d2f5d6
Maksim <crossstar66@gmail.com>
2024-06-22 00:38:51 +0500

    rehub

--- database/migrations/2024_06_10_192736_create_task_users_table.php
+++ database/migrations/2024_06_10_192736_create_task_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('task_id');
+            $table->integer('notification_status')->default(0);
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('task_id')->references('id')->on('tasks');
